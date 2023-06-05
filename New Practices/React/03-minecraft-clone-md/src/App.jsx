2a9f2caf506166c148740bbbcdd015604f5279c4
Gilbert Acosta <gisawebdev@gmail.com>
2023-06-05 16:51:57 -0400

    update

---
+++ New Practices/React/03-minecraft-clone-md/src/App.jsx
@@ -0,0 +1,23 @@
+import {Physics} from '@react-three/cannon';
+import {Sky} from '@react-three/drei';
+import {Canvas} from '@react-three/fiber';
+import {Ground} from './components/Ground';
+import {FPV as Fpv} from './components/FPV';
+import { Player } from './components/Player';
+
+const App = () => {
+	return (
+		<Canvas>
+			<Sky sunPosition={[100, 100, 20]} />
+
+			<ambientLight intensity={0.5} />
+			<Fpv />
+			<Physics>
+				<Player/>
+				<Ground />
+			</Physics>
+		</Canvas>
+	);
+};
+
+export default App;
---
+++ New Practices/React/03-minecraft-clone-md/src/images/textures.js
@@ -0,0 +1,35 @@
+import { NearestFilter, RepeatWrapping, TextureLoader } from 'three';
+import { grassImg, dirtImg, logImg, glassImg, woodImg } from './images';
+
+const grassTexture = new TextureLoader().load(grassImg);
+const dirtTexture = new TextureLoader().load(dirtImg);
+const logTexture = new TextureLoader().load(logImg);
+const glassTexture = new TextureLoader().load(glassImg);
+const woodTexture = new TextureLoader().load(woodImg);
+
+const groundTexture = new TextureLoader().load(grassImg);
+
+groundTexture.wrapS = RepeatWrapping;
+groundTexture.wrapT = RepeatWrapping;
+
+const textures = [
+	grassTexture,
+	dirtTexture,
+	logTexture,
+	glassTexture,
+	woodTexture,
+	groundTexture,
+];
+
+textures.forEach((texture) => {
+	texture.magFilter = NearestFilter;
+});
+
+export {
+	groundTexture,
+	grassTexture,
+	dirtTexture,
+	logTexture,
+	glassTexture,
+	woodTexture,
+};
